e88b8d1efb69e960498eaf5815af55e98a87da18
PatreewIgor <patreewigor@yandex.ru>
2016-09-11 21:27:29 +0300

    preparing example

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+	
   def index
   end
+
+  def mounting
+  end
+
+  def telephone
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'home#index'
+  get 'mounting' => 'home#mounting'
+  get 'telephone' => 'home#telephone'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
